8c23b5f18d8d3e9bcbdb461dde2c8d971bb414c3
Salim Hussaini <salimabdu008@gmail.com>
2021-08-10 14:47:56 +0100

    Project setup; Initial push

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+    const [isClicked, setIsClicked] = useState(false)
+    const [serverBody, setServerBody] = useState('')
+
+    const callBackendAPI = async () => {
+        setIsClicked(true)
+        const res = await fetch('/api/express_backend')
+        const body = await res.json()
+
+        if (res.status !== 200) {
+            throw Error(body.message)
+        }
+        setServerBody(body.express)
+    }
+    
+    // useEffect(() => {
+    //     callBackendAPI()
+    // })
+
+    return (
+        <>
+            <div>{isClicked ? serverBody : ""}</div>
+            {!isClicked ? <div className="cursor-pointer bg-gray-200 inline-block px-2 py-1 hover:bg-gray-400 rounded-md"
+                onClick={callBackendAPI}>
+                Click Me!!
+            </div> : ''}
+        </>
+    );
+}
+
+export default App;
