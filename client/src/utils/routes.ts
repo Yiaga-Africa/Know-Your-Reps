0c5e7b4177b0d03c09272e46cff3358e79934e73
Salim Hussaini <shussaini@yiaga.org>
2021-08-17 12:23:46 +0100

    added routes

---
+++ client/src/utils/routes.ts
@@ -0,0 +1,19 @@
+const DASHBOARD = "/"
+const LEGISLATORS = "/legislators"
+const STATE_LEGISLATORS = "/legislators/state"
+const FEDERAL_LEGISLATORS = "/legislators/federal"
+const SPECIFIC_LEGISLATOR = "/legislators/:id"
+const BILLS = "/bills"
+const SPECIFIC_BILL = "/bills/:id"
+const ANALYSIS = "/analysis"
+
+export {
+    DASHBOARD,
+    LEGISLATORS,
+    SPECIFIC_LEGISLATOR,
+    BILLS,
+    SPECIFIC_BILL,
+    STATE_LEGISLATORS,
+    FEDERAL_LEGISLATORS,
+    ANALYSIS,
+}
