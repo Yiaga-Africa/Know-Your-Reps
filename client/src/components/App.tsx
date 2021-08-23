ac58282b7e6337f457d4ee7e3cd88f69f9d50df4
Salim Hussaini <shussaini@yiaga.org>
2021-08-23 09:21:27 +0100

    added specific state route

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import DashboardPage from '../pages/DashboardPage'
-import * as Routes from '../utils/routes'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import LegislatorsPage from '../pages/LegislatorsPage'
+import React, { useState } from "react"
+import { Route, Switch } from "react-router-dom"
+import * as Routes from "../utils/routes"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { far } from "@fortawesome/free-regular-svg-icons"
+
+import DashboardPage from "../pages/DashboardPage"
+import LegislatorsPage from "../pages/LegislatorsPage"
+import StateInfoPage from "../pages/StateInfoPage"
 
 const App = () => {
     // const [isClicked, setIsClicked] = useState(false)
@@ -32,9 +34,17 @@ const App = () => {
     return (
         <Switch>
             <Route exact path={Routes.DASHBOARD} component={DashboardPage} />
-            <Route path={Routes.LEGISLATORS} component={LegislatorsPage}/>
+            <Route
+                exact
+                path={Routes.LEGISLATORS}
+                component={LegislatorsPage}
+            />
+            <Route
+                path={`${Routes.STATE_INFO}/:stateId`}
+                component={StateInfoPage}
+            />
         </Switch>
-    );
+    )
 }
 
-export default App;
+export default App
