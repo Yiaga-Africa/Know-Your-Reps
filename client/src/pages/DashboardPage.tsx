af11db4d774684957e3ade0db364016086bfa9a5
Folorunshosamuel <sfolorunsho@yiaga.org>
2021-08-30 21:09:17 +0100

    Update Dashboard

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -82,7 +82,7 @@ const DashboardPage = () => {
                     ))}
 
                     <Link
-                        to="https://yiagaafricadatahub.org/analysis"
+                        to="/AnalysisPage"
                         className="flex w-56 shadow-md hover:shadow-lg justify-center items-center bg-kyl-green text-white"
                     >
                         <div className="flex flex-col mr-8 font-semibold text-lg">
